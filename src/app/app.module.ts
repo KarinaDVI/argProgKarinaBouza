cece3970b4b7576e53db42b64b03985bcd516b7b
@KarinaDVI <kbbouza@gmail.com>
2022-03-06 04:03:02 -0300

    6 marzo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    RedesSocialesComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
